e89a40d2fede27a4b921bbb06af6328d8aa72f63
varmabudharaju <sairam.vzf33@gmail.com>
2026-03-27 18:41:16 -0700

    chore: scaffold web UI with React, Vite, Tailwind

---
+++ web/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './theme.tsx';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>,
+);
